cee9deb0f6858aeb08425ea32b94126a5642aa42
huangpengbo <huangpengbo@xinyu100.com>
2020-05-10 14:59:57 +0800

    fix bugs

--- tars/transport/tcphandler.go
+++ tars/transport/tcphandler.go
@@ -256,7 +256,7 @@ func (h *tcpHandler) recv(connSt *connInfo) {
 				currBuffer = nil
 				break
 			}
-			zaplog.Errorf("parse package error %s %v", conn.RemoteAddr(), err)
+			zaplog.Error("parse package error %s %v", zap.Any("RemoteAddr", conn.RemoteAddr()), zap.Error(err))
 			return
 		}
 	}
